9f9ad47e697b6d963ae3fb5c497b9a5c5a42dd8f
Subash A <subash@SUI-C0044-X0.local>
2022-07-20 10:25:25 +0530

    Improve parser and reader by abstracting the reader

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,10 +1,26 @@
 package parser
 
-type UserInput struct {
+import (
+	"strconv"
+	"strings"
+)
+
+type Expression struct {
 	operator string
 	operand  float64
 }
 
-func Parse() UserInput {
-	return UserInput{}
+func Parse(inputString string) Expression {
+	inputValue := Expression{}
+	inputValue.operator = strings.Split(inputString, " ")[0]
+	inputValue.operand = ConvertToFloat(strings.Split(inputString, " ")[1])
+	return inputValue
+}
+
+func ConvertToFloat(operandString string) float64 {
+	operand, err := strconv.ParseFloat(operandString, 64)
+	if err != nil {
+		panic("Error: Wrong Operand")
+	}
+	return operand
 }
--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -6,8 +6,41 @@ import (
 )
 
 func TestParser(t *testing.T) {
-	t.Run("should return a entity with operand and operator", func(t *testing.T) {
-		assert.IsType(t, UserInput{}, Parse())
+	t.Run("should return an entity with operand and operator", func(t *testing.T) {
+		assert.IsType(t, Expression{}, Parse("add 5"))
 	})
 
+	t.Run("should parse the read value as a operand and operator", func(t *testing.T) {
+		parser := Parse("add 5")
+		assert.IsType(t, "", parser.operator)
+		assert.IsType(t, 5.0, parser.operand)
+	})
+
+	t.Run("should parse the 'subtract 5' as operator-subtract and operand-5.0", func(t *testing.T) {
+		parser := Parse("subtract 5")
+		assert.Equal(t, "subtract", parser.operator)
+		assert.Equal(t, 5.0, parser.operand)
+	})
+
+	t.Run("should panic if wrong operand is provided except a number", func(t *testing.T) {
+		assert.Panics(t, func() {
+			Parse("subtract 5.a")
+		})
+	})
+}
+
+func TestConvertToFloat(t *testing.T) {
+	t.Run("should convert the string 10 to 10.0", func(t *testing.T) {
+		assert.Equal(t, 10.0, ConvertToFloat("10"))
+	})
+
+	t.Run("should convert the string -10 to -10.0", func(t *testing.T) {
+		assert.Equal(t, -10.0, ConvertToFloat("-10"))
+	})
+
+	t.Run("should panic if string cannot be converted to a float", func(t *testing.T) {
+		assert.Panics(t, func() {
+			Parse("abc")
+		})
+	})
 }
